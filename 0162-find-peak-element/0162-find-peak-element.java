9ebeda220ca129d55ec3faa5c8099a56f8c48d97
bhargavvenkatasai21-cpu <bhargavvenkatasai21@gmail.com>
2026-05-29 15:37:29 +0530

    Time: 0 ms (100%), Space: 44.4 MB (5.3%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int max=Integer.MIN_VALUE;
+         if(nums.length==0){
+            
+            return 0;
+         }
+
+        int index=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]>max){
+                max=nums[i];
+                index=i;
+            }
+            
+        }
+        return index;
+    }
+}
